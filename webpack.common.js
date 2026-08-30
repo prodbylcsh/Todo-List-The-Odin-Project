cc8a7a2667b6a70ee8145a215fa2e7c6f6685322
Lukáš Schöbel <107074516+prodbylcsh@users.noreply.github.com>
2026-08-30 11:32:07 +0200

    fixing build config

--- webpack.common.js
+++ webpack.common.js
@@ -14,9 +14,27 @@ export default {
             template: './src/index.html',
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                exclude: path.resolve(__dirname, 'src/index.html'),
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: './',
         clean: true,
     },
 };
